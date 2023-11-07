2f79898cb7db9a69cba46efa62bf0fef3ced764b
juanpabotero <juanpabotero1@gmail.com>
2023-11-07 09:35:40 -0500

    ad comments

--- src/seed/seed.controller.ts
+++ src/seed/seed.controller.ts
@@ -1,10 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { SeedService } from './seed.service';
 
+// indica que este controlador se encargará de las rutas que comiencen con /seed
 @Controller('seed')
 export class SeedController {
   constructor(private readonly seedService: SeedService) {}
 
+  // indica que este método se encargará de las peticiones GET a la ruta /seed
+  // si no se especifica el decorador @Get() se asume que es un GET
+  // si hay dos métodos con el mismo decorador y la misma ruta, se ejecuta el primero
   @Get()
   runSeed() {
     return this.seedService.populateDB();
